65708119d59b60e4dddd780200db1e3bc7d882eb
ROSS  DALY <14359691@IT.NUIGALWAY.IE>
2017-10-04 16:25:07 +0100

    Update to console output

--- CT417Assignment1PartB/src/main/java/TestDriver.java
+++ CT417Assignment1PartB/src/main/java/TestDriver.java
@@ -2,20 +2,21 @@
 import com.mycompany.ct417assignment1.Module;
 import com.mycompany.ct417assignment1.Programme;
 import com.mycompany.ct417assignment1.Student;
+import static java.lang.String.format;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import org.joda.time.DateTime;
 import java.util.ArrayList;
 import java.util.Date;
 
 /**@author 14359691 */
 public class TestDriver {
 
-    public static void main(String args[]){
+    public static void main(String args[]) throws ParseException{
         //create two arraylist with generics of the Student class
         ArrayList<Student> studentList = new ArrayList<Student>();
         ArrayList<Student> studentList2 = new ArrayList<Student>();
 
-        ArrayList<Student> allStudents = new ArrayList<Student>();  //contains all students
-        
         //creating four instances of type student and adding first two to the studentList...
         Student student1 = new Student("Tom", 30, "21/10/86", "12345678");
         studentList.add(student1);
@@ -28,27 +29,33 @@ public class TestDriver {
         studentList2.add(student4);
         
         //add all students to an arrayList containing all
+        ArrayList<Student> allStudents = new ArrayList<Student>();  //contains all students
         allStudents.add(student1);
         allStudents.add(student2);
         allStudents.add(student3);
         allStudents.add(student4);
         
-        // creating two modules... sending a different list of students to both
-        Module mod1 = new Module("Arts Stuff","BS101",studentList);
-        Module mod2 = new Module("Time, how to waste it","BS102",studentList2);
+        // creating three modules... sending a different list of students for two and one with all
+        Module mod1 = new Module("Arts Stuff","BS101",allStudents);
+        Module mod2 = new Module("Time, how to waste it","BS102",studentList);
+        Module mod3 = new Module("Dead people from history","BS103",studentList2);
         
         ArrayList<Module> modList = new ArrayList<Module>();
         modList.add(mod1);
         modList.add(mod2);
+        modList.add(mod3);
         
-        Date start = new Date(118,9,5);
-        Date end = new Date(122,5,17);
+        //creating the two joda DateTimes to represent the start and end dates
+        SimpleDateFormat format = new SimpleDateFormat("dd/MM/yyyy");
+        String srt="5/9/2018";
+        String ed="17/5/2022";
+        Date d1=format.parse(srt);
+        Date d2=format.parse(ed);
+        DateTime start = new DateTime(d1);
+        DateTime end = new DateTime(d2);
 
         Programme prog1 = new Programme("Arts with time",modList,start,end);
-        //String courseName, ArrayList modules, Date startDate, Date endDate
 
-        
-    //A list of all the students, their assigned modules and the course they are registered for should be printed to the console. 
         ArrayList<Module> modules = prog1.getModules();
 
         System.out.println("Program name: "+prog1.getCourseName());
@@ -56,7 +63,6 @@ public class TestDriver {
             System.out.println("  Module ID: "+m.getCourseID()+"\n  Module name: "+m.getModuleName());
             System.out.println("  Students taking course: "+m.getStudentList()+"\n");
         }
-
     }
     
 }
